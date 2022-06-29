7079c8838e8447987a897a8e8beaeb043aca642f
Arseniy <queezz@gmail.com>
2022-06-29 17:00:19 +0900

    sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 
-
 sys.path.insert(0, os.path.abspath(".."))
 
 
@@ -29,7 +28,7 @@ author = "A.K."
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "sphinx.ext.githubpages"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
