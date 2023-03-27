3853450a9cbd699fb87406c006a03195f3b885c7
Arseniy <queezz@gmail.com>
2023-03-27 19:14:17 +0900

    add: convert Torr to Pa

--- aklab/constants.py
+++ aklab/constants.py
@@ -4,3 +4,25 @@ package constants
 import os
 
 package_directory = os.path.dirname(os.path.abspath(__file__))
+
+
+def torr_to_pa(pressure, verbose=True):
+    """
+    Convert pressure from Torr to Pa
+
+    Parameters
+    ----------
+    pressure: float
+        pressure in Torr
+    verbose: bool
+        True to print formatted result :.2e
+    Returns
+    -------
+    pressure in Pa
+    """
+    from scipy.constants import torr
+
+    pressure = pressure * torr  # in Pa
+    if verbose:
+        print(f"{pressure:.2e} Pa")
+    return pressure
